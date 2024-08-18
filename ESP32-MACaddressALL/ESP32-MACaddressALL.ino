69eb14154e4688d02023374b928586243542aee1
Sai Krishna Mulpuru <saimulpuru@gmail.com>
2024-08-18 18:47:45 +0530

    Add files via upload

---
+++ ESP32-MACaddressALL/ESP32-MACaddressALL.ino
@@ -0,0 +1,43 @@
+#include <WiFi.h>
+
+void setup(){
+  Serial.begin(115200);
+  
+  // Variable to store the MAC address
+  uint8_t baseMac[6];
+  
+  // Get MAC address of the WiFi station interface
+  esp_read_mac(baseMac, ESP_MAC_WIFI_STA);
+  Serial.print("Station MAC: ");
+  for (int i = 0; i < 5; i++) {
+    Serial.printf("%02X:", baseMac[i]);
+  }
+  Serial.printf("%02X\n", baseMac[5]);
+  
+  // Get the MAC address of the Wi-Fi AP interface
+  esp_read_mac(baseMac, ESP_MAC_WIFI_SOFTAP);
+  Serial.print("SoftAP MAC: ");
+  for (int i = 0; i < 5; i++) {
+    Serial.printf("%02X:", baseMac[i]);
+  }
+  Serial.printf("%02X\n", baseMac[5]);
+  
+  // Get the MAC address of the Bluetooth interface
+  esp_read_mac(baseMac, ESP_MAC_BT);
+  Serial.print("Bluetooth MAC: ");
+  for (int i = 0; i < 5; i++) {
+    Serial.printf("%02X:", baseMac[i]);
+  }
+  Serial.printf("%02X\n", baseMac[5]);
+
+  // Get the MAC address of the Ethernet interface
+  esp_read_mac(baseMac, ESP_MAC_ETH);
+  Serial.print("Ethernet MAC: ");
+  for (int i = 0; i < 5; i++) {
+    Serial.printf("%02X:", baseMac[i]);
+  }
+  Serial.printf("%02X\n", baseMac[5]);
+}
+ 
+void loop(){
+}
