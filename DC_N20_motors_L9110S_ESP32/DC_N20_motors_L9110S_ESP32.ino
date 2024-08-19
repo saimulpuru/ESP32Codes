798540c8ada92a11452e4fab202a9fe13871158e
Sai Krishna Mulpuru <saimulpuru@gmail.com>
2024-08-19 19:13:15 +0530

    Add files via upload

---
+++ DC_N20_motors_L9110S_ESP32/DC_N20_motors_L9110S_ESP32.ino
@@ -0,0 +1,29 @@
+const int lm1 = 25;
+const int lm2 = 26;
+const int rm1 = 32;
+const int rm2 = 33;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(lm1, OUTPUT);
+pinMode(lm2, OUTPUT);
+pinMode(rm1, OUTPUT);
+pinMode(rm2, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(lm1, HIGH);
+digitalWrite(lm2, LOW);
+digitalWrite(rm1, LOW);
+digitalWrite(rm2, HIGH);
+
+delay(1000);
+
+digitalWrite(lm1, LOW);
+digitalWrite(lm2, HIGH);
+digitalWrite(rm1, HIGH);
+digitalWrite(rm2, LOW);
+
+delay(1000);
+}
